4f0a11619e91c0504cfeecd6ae375fbb94876662
irfanmuh12 <muhammad.irfan.im96@gmail.com>
2022-10-14 02:25:09 +0900

    Add all my files

---
+++ js/struktur-organisasi/struktur.js
@@ -0,0 +1,409 @@
+const struktur = [
+    { 
+        id: "1", 
+        tags:["DIREKTUR"], 
+        nama: "dr. Prihatin Iman Nugroho, M.kes Sp P", 
+        ttl: "	SEMARANG / 28 Agustus 1971",
+        agama: "ISLAM",
+        status: "PNS",
+        jenis_kepegawaian: "PNS Daerah Otonom",
+        kedudukan_pegawai: "PEGAWAI AKTIF",
+        pendidikan_terakhir: "	STRATA 2 (S2) MAGISTER KESEHATAN",
+        jabatan: "DIREKTUR", 
+        pangkat: "Penata Tingkat I", 
+        img: "images/team/1.jpg",
+        sex: "laki-laki",
+        nip: "197108282006041016",
+        jabatan_struktural: {
+            jabatan_saat_ini : "struktural",
+            ditetapkan_oleh : "gubernur maluku",
+            nama_jabatan : "DIREKTUR",
+            tmt_jabatan : "26-07-2019",
+            eselon : "III.A"
+        },
+        riwayat_jabatan: [
+            {
+                no: "1",
+                nama_jabatan: "DOKTER MUDA",
+                tmt_jab: "-"
+            },
+            {
+                no: "2",
+                nama_jabatan: "TUGAS BELAJAR",
+                tmt_jab: "31-12-2011"
+            },
+            {
+                no: "3",
+                nama_jabatan: "DOKTER PERTAMA",
+                tmt_jab: "16-05-2014"
+            },
+            {
+                no: "4",
+                nama_jabatan: "DOKTER MUDA",
+                tmt_jab: "22-09-2014"
+            },
+            {
+                no: "5",
+                nama_jabatan: "DOKTER AHLI MADYA",
+                tmt_jab: "29-08-2016"
+            },
+            {
+                no: "6",
+                nama_jabatan: "KEPALA BIDANG PELAYANAN",
+                tmt_jab: "11-08-2017"
+            },
+            {
+                no: "7",
+                nama_jabatan: "WAKIL DIREKTUR PELAYANAN",
+                tmt_jab: "26-07-2019"
+            }
+        ], 
+        riwayat_tanda_jasa: [
+            {
+                no: "1",
+                nama_penghargaan: "SATYALANCANA KARYA SATYA X TAHUN",
+                tahun: "2020",
+                asal: "PRESIDEN RI"
+            }
+        ],
+        riwayat_pendidikan_umum: [
+            {
+                no: "1",
+                tingkat: "SD",
+                jurusan: "-",
+                nama_sekolah: "SDN PETOMPON II",
+                lulus: "1984"
+            },
+            {
+                no: "2",
+                tingkat: "SLTP",
+                jurusan: "-",
+                nama_sekolah: "SMPN 5",
+                lulus: "1987"
+            },
+            {
+                no: "3",
+                tingkat: "SLTA",
+                jurusan: "A1",
+                nama_sekolah: "SMAN 6",
+                lulus: "1990"
+            },
+            {
+                no: "4",
+                tingkat: "STRATA 1 (S1)",
+                jurusan: "FAK. KEDOKTERAN",
+                nama_sekolah: "	UNISSULA",
+                lulus: "2002"
+            },
+            {
+                no: "5",
+                tingkat: "STRATA 2 (S2)",
+                jurusan: "MAGISTER KESEHATAN",
+                nama_sekolah: "UNIV SEBELAS MARET",
+                lulus: "2013"
+            }
+        ]
+    },
+
+    { 
+        id: "2", 
+        pid: "2", 
+        nama: "dr. Yenny Yulianti, Sp.Pk", 
+        ttl: "SEMARANG / 19 Juli 1984",
+        agama: "ISLAM",
+        status: "PNS",
+        jenis_kepegawaian: "PNS Daerah Otonom",
+        kedudukan_pegawai: "PEGAWAI AKTIF",
+        pendidikan_terakhir: "STRATA 2 (S2) PATOLOGI KLINIK",
+        jabatan: "KABID PENUNJANG", 
+        pangkat:"Pembina", 
+        img: "images/team/2.jpg" ,
+        sex: "PEREMPUAN",
+        nip: "198407192009032009",
+        jabatan_struktural: {
+            jabatan_saat_ini : "struktural",
+            ditetapkan_oleh : "gubernur maluku",
+            nama_jabatan : "KABID PENUNJANG",
+            tmt_jabatan : "07-08-2020",
+            eselon : "III.B"
+        },
+        riwayat_jabatan: [
+            {
+                no: "1",
+                nama_jabatan: "DOKTER PERTAMA",
+                tmt_jab: "-"
+            },
+            {
+                no: "2",
+                nama_jabatan: "DOKTER MUDA",
+                tmt_jab: "21-05-2012"
+            },
+            {
+                no: "3",
+                nama_jabatan: "TUGAS BELAJAR",
+                tmt_jab: "03-01-2013"
+            },
+            {
+                no: "4",
+                nama_jabatan: "DOKTER AHLI MUDA",
+                tmt_jab: "20-12-2017"
+            },
+            {
+                no: "5",
+                nama_jabatan: "	DOKTER AHLI MADYA",
+                tmt_jab: "11-05-2018"
+            },
+            {
+                no: "6",
+                nama_jabatan: "KABID PENUNJANG",
+                tmt_jab: "07-08-2020"
+            },
+        ],
+        riwayat_tanda_jasa: [
+            {
+                no: "-",
+                nama_penghargaan: "-",
+                tahun: "-",
+                asal: "-"
+            }
+        ], 
+        riwayat_pendidikan_umum: [
+            {
+                no: "1",
+                tingkat: "SD",
+                jurusan: "-",
+                nama_sekolah: "SDN TAMAN MALUKU 01",
+                lulus: "1996"
+            },
+            {
+                no: "2",
+                tingkat: "SLTP",
+                jurusan: "-",
+                nama_sekolah: "SMPN 2",
+                lulus: "1999"
+            },
+            {
+                no: "3",
+                tingkat: "SLTA",
+                jurusan: "IPA",
+                nama_sekolah: "SMAN 3",
+                lulus: "2002"
+            },
+            {
+                no: "4",
+                tingkat: "STRATA 1 (S1)",
+                jurusan: "KEDOKTERAN UMUM",
+                nama_sekolah: "UNDIP",
+                lulus: "2006"
+            },
+            {
+                no: "5",
+                tingkat: "STRATA 1 (S1)",
+                jurusan: "PROFESI",
+                nama_sekolah: "UNDIP",
+                lulus: "2008"
+            },
+            {
+                no: "6",
+                tingkat: "STRATA 2 (S2)",
+                jurusan: "	PATOLOGI KLINIK",
+                nama_sekolah: "UNDIP",
+                lulus: "2017"
+            }
+        ]
+    },
+	
+    { 
+        id: "3", 
+        pid: "2", 
+        nama: "dr. Retno Wuryani", 
+        ttl: "CILACAP / 21 Juni 1971",
+        agama: "ISLAM",
+        status: "PNS",
+        jenis_kepegawaian: "PNS Daerah Otonom",
+        kedudukan_pegawai: "PEGAWAI AKTIF",
+        pendidikan_terakhir: "STRATA 1 (S1) DOKTER UMUM",
+        jabatan: "KABID PELAYANAN", 
+        pangkat: "Pembina", 
+        img: "images/team/team1.jpg" ,
+        sex: "PEREMPUAN",
+        nip: "197106212006042018",
+        jabatan_struktural: {
+            jabatan_saat_ini : "struktural",
+            ditetapkan_oleh : "gubernur maluku",
+            nama_jabatan : "KABID PELAYANAN",
+            tmt_jabatan : "26-07-2019",
+            eselon : "III.B"
+        },
+        riwayat_jabatan: [
+            {
+                no: "1",
+                nama_jabatan: "DOKTER PERTAMA",
+                tmt_jab: "13-06-2008"
+            },
+            {
+                no: "2",
+                nama_jabatan: "DOKTER MUDA",
+                tmt_jab: "01-04-2009"
+            },
+            {
+                no: "3",
+                nama_jabatan: "KASI PELAYANAN RAWAT INAP",
+                tmt_jab: "10-03-2014"
+            },
+            {
+                no: "4",
+                nama_jabatan: "DOKTER MADYA",
+                tmt_jab: "17-03-2014"
+            },
+            {
+                no: "5",
+                nama_jabatan: "KABID PELAYANAN",
+                tmt_jab: "26-07-2019"
+            }
+        ],
+        riwayat_tanda_jasa: [
+            {
+                no: "1",
+                nama_penghargaan: "SATYALANCANA KARYA SATYA X TAHUN",
+                tahun: "2020",
+                asal: "PRESIDEN RI"
+            }
+        ], 
+        riwayat_pendidikan_umum: [
+            {
+                no: "1",
+                tingkat: "SD",
+                jurusan: "-",
+                nama_sekolah: "SD MARSUDIRINI",
+                lulus: "1984"
+            },
+            {
+                no: "2",
+                tingkat: "SLTP",
+                jurusan: "-",
+                nama_sekolah: "SMPN 6",
+                lulus: "1987"
+            },
+            {
+                no: "3",
+                tingkat: "SLTA",
+                jurusan: "-",
+                nama_sekolah: "SMAN 5",
+                lulus: "1990"
+            },
+            {
+                no: "4",
+                tingkat: "STRATA 1 (S1)",
+                jurusan: "KEDOKTERAN UMUM",
+                nama_sekolah: "UNISSULA",
+                lulus: "2000"
+            }
+        ]
+    },
+    
+    { 
+        id: "4", 
+        pid: "2", 
+        nama: "Kurnia Yuliastuti, Skep.Ners, M.kep", 
+        ttl: "BOYOLALI / 18 Juli 1975",
+        agama: "ISLAM",
+        status: "PNS",
+        jenis_kepegawaian: "PNS Daerah Otonom",
+        kedudukan_pegawai: "PEGAWAI AKTIF",
+        pendidikan_terakhir: "STRATA 1 (S1) KEPERAWATAN",
+        jabatan: "KABID PERAWATAN", 
+        pangkat:"Penata Tingkat I", 
+        img: "images/team/4.jpg" ,
+        sex: "PEREMPUAN",
+        nip: "197507181998032002",
+        jabatan_struktural: {
+            jabatan_saat_ini : "struktural",
+            ditetapkan_oleh : "gubernur maluku",
+            nama_jabatan : "KABID KEPERAWATAN",
+            tmt_jabatan : "	26-07-2019",
+            eselon : "III.B"
+        },
+        riwayat_jabatan: [
+            {
+                no: "1",
+                nama_jabatan: "STAF AKADEMI KEPERAWATAN DI WONOSOBO",
+                tmt_jab: "-"
+            },
+            {
+                no: "2",
+                nama_jabatan: "KASI AKDEMIK DAN PENELITIAN",
+                tmt_jab: "10-03-2014"
+            },
+            {
+                no: "3",
+                nama_jabatan: "KASI PENUNJANG MEDIK",
+                tmt_jab: "01-01-2017"
+            },
+            {
+                no: "4",
+                nama_jabatan: "KABID KEPERAWATAN",
+                tmt_jab: "26-07-2019"
+            }
+        ],
+        riwayat_tanda_jasa: [
+            {
+                no: "-",
+                nama_penghargaan: "-",
+                tahun: "-",
+                asal: "-"
+            }
+        ], 
+        riwayat_pendidikan_umum: [
+            {
+                no: "1",
+                tingkat: "SD",
+                jurusan: "-",
+                nama_sekolah: "SDN TEGALSARI I",
+                lulus: "1987"
+            },
+            {
+                no: "2",
+                tingkat: "SLTP",
+                jurusan: "BIOLOGI",
+                nama_sekolah: "SMPN KARANGGEDE",
+                lulus: "1990"
+            },
+            {
+                no: "3",
+                tingkat: "SLTA",
+                jurusan: "IPA",
+                nama_sekolah: "SMAN I",
+                lulus: "1993"
+            },
+            {
+                no: "4",
+                tingkat: "DIPLOMA III",
+                jurusan: "KESEHATAN",
+                nama_sekolah: "AKPER MUH SURAKARTA",
+                lulus: "1996"
+            },
+            {
+                no: "5",
+                tingkat: "STRATA 1 (S1)",
+                jurusan: "KEPERAWATAN",
+                nama_sekolah: "UNDIP",
+                lulus: "2003"
+            },
+            {
+                no: "6",
+                tingkat: "STRATA 1 (S1)",
+                jurusan: "	PROFESI KLINIK",
+                nama_sekolah: "UNDIP",
+                lulus: "2004"
+            },
+            {
+                no: "7",
+                tingkat: "STRATA 2 (S2)",
+                jurusan: "KEPERAWATAN",
+                nama_sekolah: "Universitas Indonesia",
+                lulus: "2009"
+            }
+        ]
+    }
+];
